730d7fa70be899b49f1fe61ca681d469b389e256
LucaFasone <luca.fasone15@gmail.com>
2024-11-03 23:19:44 +0100

    Update schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from 'drizzle-orm';
 import { int, mysqlTable, serial, uniqueIndex, varchar, text, bigint, foreignKey } from 'drizzle-orm/mysql-core';
 import { sql } from 'drizzle-orm/sql';
 
@@ -18,26 +19,41 @@ export const messageTable = mysqlTable('message', {
     id: serial().primaryKey(),
     message: text(),
     sender_id: bigint({ mode: "bigint", unsigned: true }).notNull(),
-    recipient_id: bigint({ mode: "bigint", unsigned: true }).notNull(),
+    recipient_id: bigint({ mode: "bigint", unsigned: true }),
     roomId: bigint({ mode: "bigint", unsigned: true })
 }, (table) => ({
     fk: foreignKey({
         name: "message_sender_id_users_id_fk",
         columns: [table.sender_id],
         foreignColumns: [usersTable.id],
-    })
-        .onDelete("cascade")
-        .onUpdate("cascade"),
-    foreignKey: foreignKey({
+    }).onDelete("cascade").onUpdate("cascade"),
+    fk2: foreignKey({
         name: "message_recipient_id_users_id_fk",
         columns: [table.recipient_id],
         foreignColumns: [usersTable.id],
-    })
-        .onDelete("cascade")
-        .onUpdate("cascade"),
-    foreignKeyy: foreignKey({
+    }).onDelete("cascade").onUpdate("cascade"),
+    fk3: foreignKey({
         name: "message_room_id_room_id_fk",
         columns: [table.roomId],
         foreignColumns: [roomTable.id],
-    })
+    }).onDelete("cascade").onUpdate("cascade"),
 }))
+
+export const userInRoom = mysqlTable('user_in_room', {
+    id: serial().primaryKey(),
+    userId: bigint({ mode: "bigint", unsigned: true }).notNull(),
+    roomId: bigint({ mode: "bigint", unsigned: true }).notNull()
+}, (table) => ({
+    fk: foreignKey({
+        name: "user_in_room_user_id_users_id_fk",
+        columns: [table.userId],
+        foreignColumns: [usersTable.id],
+    }).onDelete("cascade").onUpdate("cascade"),
+    fk2: foreignKey({
+        name: "user_in_room_room_id_room_id_fk",
+        columns: [table.roomId],
+        foreignColumns: [roomTable.id],
+    }).onDelete("cascade").onUpdate("cascade"),
+}))
+//maybe its not needed to use a relations function
+// export const messageRelations = relations(messageTable, ({ one, many }) => ({}))
